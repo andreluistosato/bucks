e6d3078d19d8e88f843613b724338572e076f92c
André Tosato <adacruz1@its.jnj.com>
2022-05-17 11:46:04 -0300

    Adding a successful unit test

--- src/test/java/com/andreluistosato/bucks/BucksApplicationTests.java
+++ src/test/java/com/andreluistosato/bucks/BucksApplicationTests.java
@@ -1,13 +1,19 @@
 package com.andreluistosato.bucks;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BucksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
+  @Test
+  void checkIfOnePlusOneIsTwo() {
+    assertThat(1 + 1, Matchers.is(2));
+  }
 }
