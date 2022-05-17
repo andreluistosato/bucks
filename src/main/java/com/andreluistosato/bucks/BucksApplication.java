93b44812f14b20f09455ea8356ab4a799e732f77
André Tosato <adacruz1@its.jnj.com>
2022-05-17 07:52:52 -0300

    Initial commit

---
+++ src/main/java/com/andreluistosato/bucks/BucksApplication.java
@@ -0,0 +1,13 @@
+package com.andreluistosato.bucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreluistosato/bucks/BucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreluistosato.bucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
